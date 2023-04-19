353f5515c99eabc8e65419c98928f09fa4e742ba
yaredghebre <yared2391@gmail.com>
2023-04-19 06:45:40 -0300

    Add: Complete (updated)

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { reactive} from "vue";
 
 export const store = reactive({
     cards: [],
-    apiURL: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0",
+    apiURL: "https://db.ygoprodeck.com/api/v7/cardinfo.php",
     loading: false,
     selectedOption: ""
 });
